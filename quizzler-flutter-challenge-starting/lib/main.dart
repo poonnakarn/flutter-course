4028a13018b92f09433d0caec5dba3d6d5f29a56
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2019-12-19 07:03:44 +0700

    Add rFlutter alert challenge project

--- quizzler-flutter-challenge-starting/lib/main.dart
+++ quizzler-flutter-challenge-starting/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//TODO: Step 2 - Import the rFlutter_Alert package here.
 import 'quiz_brain.dart';
 
 QuizBrain quizBrain = QuizBrain();
@@ -13,7 +14,7 @@ class Quizzler extends StatelessWidget {
         backgroundColor: Colors.grey.shade900,
         body: SafeArea(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
             child: QuizPage(),
           ),
         ),
@@ -33,22 +34,26 @@ class _QuizPageState extends State<QuizPage> {
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizBrain.getCorrectAnswer();
 
-    // setState triggers build function
     setState(() {
-      quizBrain.nextQuestion();
-      if (correctAnswer == userPickedAnswer) {
-        // User correct
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
+      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+      //HINT! Step 4 Part B is in the quiz_brain.dart
+      //TODO: Step 4 Part C - reset the questionNumber,
+      //TODO: Step 4 Part D - empty out the scoreKeeper.
+
+      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
+      if (userPickedAnswer == correctAnswer) {
         scoreKeeper.add(Icon(
           Icons.check,
           color: Colors.green,
         ));
       } else {
-        // User wrong
         scoreKeeper.add(Icon(
           Icons.close,
           color: Colors.red,
         ));
       }
+      quizBrain.nextQuestion();
     });
   }
 
@@ -115,7 +120,7 @@ class _QuizPageState extends State<QuizPage> {
         ),
         Row(
           children: scoreKeeper,
-        ),
+        )
       ],
     );
   }
