458257e87a3dc75ffe514b09b27e4d36b1b91891
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2019-12-21 17:14:38 +0700

    Extract widget to class, const&final

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1F33);
+const bottomContainerColor = Color(0xFFEB1556);
 
 class InputPage extends StatefulWidget {
   @override
@@ -19,48 +24,88 @@ class _InputPageState extends State<InputPage> {
                 children: <Widget>[
                   Expanded(
                     child: ReusableCard(
-                      colour: Color(0xFF1D1F33),
+                      colour: activeCardColor,
+                      cardChild: IconContent(
+                          icon: FontAwesomeIcons.mars, text: 'MALE'),
                     ),
                   ),
                   Expanded(
                       child: ReusableCard(
-                    colour: Color(0xFF1D1F33),
+                    colour: activeCardColor,
+                    cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus, text: 'FEMALE'),
                   )),
                 ],
               ),
             ),
             Expanded(
                 child: ReusableCard(
-              colour: Color(0xFF1D1F33),
+              colour: activeCardColor,
             )),
             Expanded(
               child: Row(
                 children: <Widget>[
                   Expanded(
                       child: ReusableCard(
-                    colour: Color(0xFF1D1F33),
+                    colour: activeCardColor,
                   )),
                   Expanded(
                       child: ReusableCard(
-                    colour: Color(0xFF1D1F33),
+                    colour: activeCardColor,
                   )),
                 ],
               ),
+            ),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
             ),
           ],
         ));
   }
 }
 
+class IconContent extends StatelessWidget {
+  final String text;
+  final IconData icon;
+
+  IconContent({this.icon, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(height: 15.0),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, this.cardChild});
 
   // final = immutable property
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
