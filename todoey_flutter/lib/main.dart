05ad69ed1c3a876dc784dcdc68d68944b97ee9bf
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2020-02-02 19:37:40 +0700

    Complete all projects

--- todoey_flutter/lib/main.dart
+++ todoey_flutter/lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/screens/tasks_screen.dart';
+import 'package:todoey_flutter/models/task_data.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ChangeNotifierProvider(
+      create: (context) => TaskData(),
+      child: MaterialApp(
+        home: TasksScreen(),
+      ),
+    );
   }
 }
